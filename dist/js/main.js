f3509ac466bdffd954c1cff8e23472b67ff15711
Madz <almahdee2012@gmail.com>
2018-08-11 04:46:58 +0300

    Menu Overly

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,7 +3,6 @@ const menuBtn = document.querySelector(".menu-btn");
 const menu = document.querySelector(".menu");
 const menuNav = document.querySelector(".menu-nav");
 const menuBranding = document.querySelector(".menu-branding");
-
 const navItems = document.querySelectorAll(".nav-item");
 
 // Set Initial State of Menu
@@ -14,7 +13,7 @@ menuBtn.addEventListener("click", toggleMenu);
 function toggleMenu() {
   if (!showMenu) {
     menuBtn.classList.add("close");
-    menu.classList.add("show`");
+    menu.classList.add("show");
     menuNav.classList.add("show");
     menuBranding.classList.add("show");
     navItems.forEach(item => item.classList.add("show"));
@@ -28,6 +27,6 @@ function toggleMenu() {
     menuBranding.classList.remove("show");
     navItems.forEach(item => item.classList.remove("show"));
 
-    showMenu - false;
+    showMenu = false;
   }
 }
